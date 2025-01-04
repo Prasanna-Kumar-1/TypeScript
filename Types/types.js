291d91e1f7f26336fdf438a841b7de57131c1405
Prasanna <prasanna.kumar@outlook.com.au>
2025-01-04 23:36:27 +1100

    Initial commit

---
+++ Types/types.js
@@ -0,0 +1,13 @@
+//Type Annotations
+//string
+var movieTitle = "Titanic";
+movieTitle = "Inception";
+movieTitle.toUpperCase();
+//number
+var seatNumber = 23;
+//boolean
+var ticketConfirmed = true;
+//Type Inference
+var movieName = "Inception";
+var seatNo = 23;
+var ticketStatus = true;
