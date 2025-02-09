54217fd5f7169abde31b2de0d91f59273da0f12e
Prasanna <prasanna.kumar@outlook.com.au>
2025-02-09 21:50:23 +1100

    void return type

--- Function/voidreturntype.ts
+++ Function/voidreturntype.ts
@@ -1,3 +1,4 @@
+//return type of a function is void, it means that the function does not return any value. It is used when a function does not return any value. It is optional to specify the return type of a function. If the return type is not specified, it is considered as void.
 function printTwice(message: string): void {
   console.log(message);
   console.log(message);
