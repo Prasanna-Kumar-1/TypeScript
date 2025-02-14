612024954ba016156fd226af313bc8eccf69c6f8
Prasanna <prasanna.kumar@outlook.com.au>
2025-02-15 10:01:37 +1100

    Arrow functions

--- Function/arrowfunction.ts
+++ Function/arrowfunction.ts
@@ -1,4 +1,4 @@
-//Arrow Function
+//Arrow functions are a more concise way to write a function in TypeScript. They are also called lambda functions in other languages. Arrow functions are always anonymous functions. They are defined using a pair of parentheses, followed by an arrow token (=>), which is followed by the function body.
 const doSomething = (person: string, age: number, isFunny: boolean) => {};
 //correct
 doSomething("Prasanna", 25, true);
